afb2f4e37aa5c33f0fd8b27b9044674362c9e91d
Rio <rioten353@gmail.com>
2024-11-11 17:34:05 +0530

    Add files via upload

---
+++ node_js.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+wget https://nodejs.org/dist/v22.11.0/node-v22.11.0-linux-x64.tar.xz
+
+tar -xf node-v22.11.0-linux-x64.tar.xz
+
+export PATH="$PATH:/root/node-v22.11.0-linux-x64/bin"
+
+source ~/.bashrc
+
+
