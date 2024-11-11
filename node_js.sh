00773283b1190a9a641824b7e834da23f93e811c
Rio <rioten353@gmail.com>
2024-11-11 17:36:51 +0530

    Update node_js.sh

--- node_js.sh
+++ node_js.sh
@@ -5,7 +5,8 @@ wget https://nodejs.org/dist/v22.11.0/node-v22.11.0-linux-x64.tar.xz
 
 tar -xf node-v22.11.0-linux-x64.tar.xz
 
-export PATH="$PATH:/root/node-v22.11.0-linux-x64/bin"
+
+echo 'export PATH="$PATH:/root/node-v22.11.0-linux-x64/bin"' >> ~/.bashrc
 
 source ~/.bashrc
 
